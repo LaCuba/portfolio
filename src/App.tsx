f3bb5f3fdd63305d0afb4a2f6b9622ff20ca9896
Nick Romanov <romanov@apsolutions.com>
2021-12-12 21:28:52 +0300

    Created animations on main page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styles from "./App.module.scss"
 
 const App = () => {
+  const [widthText, setWidthText] = useState<string>('330px')
+  const [timout, setTimout] = useState<number>()
+
+  useEffect(() => {
+    if (widthText === '0px') {
+      clearTimeout(timout)
+      const timer = window.setTimeout(() => setWidthText('330px'), 800)
+      setTimout(timer)
+    } else if (widthText === '330px') {
+      clearTimeout(timout)
+      const timer = window.setTimeout(() => setWidthText('0px'), 2000)
+      setTimout(timer)
+    }
+  }, [widthText])
+
   return (
     <div className={styles.body}>
       <div className={styles.container}>
@@ -9,15 +24,41 @@ const App = () => {
         <div className={styles.app}>
           <div className={styles.info}>
             <h2 className={styles.greeting}>HI THERE ! I'M</h2>
-            <h2 className={styles.myName}>NICK ROMANOV</h2>
-            <h2 className={styles.profession}>FRONTEND-DEVELOPER</h2>
+            <h1 className={styles.myName}>NICK ROMANOV</h1>
+            <span className={styles.professionBox} style={{ width: widthText }}>
+              <h2 className={styles.profession}>FRONTEND-DEVELOPER</h2>
+            </span>
           </div>
-          <div className={styles.aboutMe}>
-            <h2 className={styles.about}>ABOUT</h2>
-            <h2 className={styles.me}>ME</h2>
+          <div className={styles.aboutMeBlock}>
+              <h2 className={styles.aboutMeBox}>
+                <span className={styles.about} data-hover={"ABOUT"}>
+                  ABOUT
+                </span>
+                <span className={styles.me} data-hover={"ME"}>
+                  ME
+                </span>
+              </h2>
           </div>
-          <div className={styles.portfolio}>fasd</div>
-          <div className={styles.message}>fasdf</div>
+          <div className={styles.portfolioBlock}>
+              <h2 className={styles.portfolioBox}>
+                <span className={styles.my} data-hover={"MY"}>
+                  MY
+                </span>
+                <span className={styles.portfolio} data-hover={"PORTFOLIO"}>
+                  PORTFOLIO
+                </span>
+              </h2>
+          </div>
+          <div className={styles.messageBlock}>
+              <h2 className={styles.messageBox}>
+                <span className={styles.get} data-hover={"GET"}>
+                  GET
+                </span>
+                <span className={styles.inTouch} data-hover={"IN TOUCH"}>
+                  IN TOUCH
+                </span>
+              </h2>
+          </div>
         </div>
       </div>
     </div>
